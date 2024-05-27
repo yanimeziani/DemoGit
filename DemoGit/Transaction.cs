68127c765ea788c53684e4c534ecc612fa32d654
Yani Meziani <yani.meziani@protonmail.com>
2024-05-27 15:03:37 -0400

    Hello

---
+++ DemoGit/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit
+{
+    public sealed class Transaction
+    {
+        public string CardNumber { get; set; }
+        public string CardHolder { get; set; }
+        public string ExpiryDate { get; set; }
+    }
+}
