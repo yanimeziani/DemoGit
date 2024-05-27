9549cd7dfbb651e12cc1c0a0640412ca51a42deb
Yani Meziani <yani.meziani@protonmail.com>
2024-05-27 15:04:53 -0400

    Credit card uses int array instead of string

--- DemoGit/Transaction.cs
+++ DemoGit/Transaction.cs
@@ -8,7 +8,7 @@ namespace DemoGit
 {
     public sealed class Transaction
     {
-        public string CardNumber { get; set; }
+        public int[] CardNumber { get; set; }
         public string CardHolder { get; set; }
         public string ExpiryDate { get; set; }
     }
